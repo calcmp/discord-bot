b19a322017b73eb03bd61580876748dae99c1e29
calcmp <calcmp@gmail.com>
2020-05-14 20:23:53 +0100

    Setup poke functionality

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-require("dotenv").config({ path: ".env" });
-const Discord = require("discord.js");
+const { Client, MessageEmbed } = require("discord.js");
+const { PREFIX, DISCORD_BOT_TOKEN } = require("./config.json");
+const { poke } = require("./src/commands/poke.js");
 
-const client = new Discord.Client();
+const client = new Client();
+const msgEmbed = new MessageEmbed();
 
 client.once("ready", () => {
   console.log("Ready!");
@@ -10,9 +12,19 @@ client.once("ready", () => {
 client.on("message", (message) => {
   console.log(message.content);
 
-  if (message.content === "!ping") {
-    message.channel.send("Pong.");
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+  const args = message.content.slice(PREFIX.length).split(" ");
+  const command = args.shift().toLowerCase();
+
+  switch (command) {
+    case "ping":
+      message.channel.send("Pong.");
+    case "poke":
+      poke(message, client, msgEmbed, args);
+    default:
+      return;
   }
 });
 
-client.login(process.env.DISCORD_BOT_KEY);
+client.login(DISCORD_BOT_TOKEN);
