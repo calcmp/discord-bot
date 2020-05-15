f385132f45360591c515988df90131282f916276
calcmp <calcmp@gmail.com>
2020-05-15 13:09:17 +0100

    All message go through bot DM instead of text chats

--- src/commands/poke.js
+++ src/commands/poke.js
@@ -1,25 +1,29 @@
-const poke = (message, client, args) => {
-  if (!args.length || !message.mentions.members.first()) {
-    return message.channel.send(
-      "Type the person who you want to poke using '@'."
+const poke = async (message, client, args) => {
+  const messageAuthor = message.author.username;
+  const userIdsMentioned = [];
+  const usernamesMentioned = [];
+
+  if (!args.length || message.content.includes("@")) {
+    return message.author.send(
+      "Type the person who you want to poke without '@'."
     );
   } else {
-    const userIdsMentioned = [];
-    const usernamesMentioned = [];
-    const messageAuthor = message.author.username;
-
-    message.mentions.users.forEach((user) => {
-      userIdsMentioned.push(user.id);
-      usernamesMentioned.push(user.username);
+    client.users.cache.forEach((user) => {
+      args.forEach((arg) => {
+        if (arg === user.username.toLowerCase()) {
+          userIdsMentioned.push(user.id);
+          usernamesMentioned.push(user.username);
+        }
+      });
     });
 
     userIdsMentioned.forEach(async (user) => {
       let recipient = await client.users.fetch(user);
       recipient.send(`${messageAuthor} has poked you!`);
     });
-    usernamesMentioned.forEach((user) =>
-      message.channel.send(`${messageAuthor} poked ${user}`)
-    );
+    usernamesMentioned.forEach((user) => {
+      message.author.send(`You poked ${user}!`);
+    });
   }
 };
 
